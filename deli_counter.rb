ec1d873fd950446c85ec95f8847edc46a2e61dbd
Seth Todd <seth.todd@outlook.com>
2020-01-27 03:48:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ def line(array)
  if array.size == 0
   puts "The line is currently empty."
   else
-  new = []
-  array.each_with_index do |name, index|
-    new << "#{index+1}. #{name}"
-    end
+    new = []
+      array.each_with_index do |name, index|
+        new << "#{index+1}. #{name}"
+      end
     puts "The line is currently: #{new.join(" ")}"
-    end
-    end
+  end
+end
